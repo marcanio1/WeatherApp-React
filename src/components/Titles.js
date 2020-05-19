a4dee715aa8f800dded0545fbe2d7f9bca4d9788
Eric Marcanio <ericmarcanio@gmail.com>
2020-05-18 19:39:39 -0500

    Inital weather app

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+    <div>
+        <h1 className="title-container__title">Weather App</h1>
+        <p className="title-container__subtitle">Enter your location to check the temp!</p>
+    </div>
+)
+
+export default Titles;
